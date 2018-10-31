2c14cfac92b6f802a105dd37e9d231630d5c2edb
Rolf Kleef <rolf@drostan.org>
2018-10-31 15:52:54 +0100

    Update webhook hooks and scripts
    Rename folder to webhook-scripts
    Remove test hook
    Add test for valid URL, return a message if not
    Add md5sum of file as mask to use in processing
    Combine in a single feedback script for 3 formats

---
+++ engine/webhook-scripts/feedback.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# taken an expression from https://mathiasbynens.be/demo/url-regex
+URLREGEX='^(https?|ftp)://[^[:space:]/$.?#].[^[:space:]]*$'
+
+# determine which output format to use
+case $2 in
+  svrl)
+    target=svrl
+    resultdir=svrl
+    resultext=svrl
+    break;;
+  json)
+    target=json
+    resultdir=json
+    resultext=json
+    break;;
+  *)
+    target=feedback
+    resultdir=dest
+    resultext=feedback.xml
+esac
+
+if echo "$1" | grep -Eq "$URLREGEX"
+then
+  cd /home 
+  mkdir -p /workspace/input
+  
+  # put the source file into /workspace/input using the md5 as name
+  tfile=$(mktemp /tmp/XXXXXXXXX)
+  wget -q -O $tfile $1
+  md5sum=$(md5sum $tfile | cut -d \  -f 1) 
+  mv $tfile /workspace/input/$md5sum.xml
+  
+  # generate and return the svrl output
+  ant $target -S -q -Dfilemask=$md5sum
+  cat /workspace/$resultdir/$md5sum.$resultext
+
+else
+  cat /home/webhook-scripts/not-url.$resultext
+fi
