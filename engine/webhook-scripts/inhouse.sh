0279f7a4b80ef02d7e4ee6a17f30ec4161a046ac
Rolf Kleef <rolf@drostan.org>
2019-01-17 15:45:20 +0100

    Fix process container problems
    Add curl to docker container
    Fix error in download URL
    Fix cleaning up files at the end
    Add check for successful download of source data

--- engine/webhook-scripts/inhouse.sh
+++ engine/webhook-scripts/inhouse.sh
@@ -13,57 +13,59 @@ basename=$1
 
 mkdir -p /workspace/input
 
-curl -X GET "$API/iati-files/download/dataworkbench-iati/$basename.xml" > /workspace/input/$basename.xml 
+HTTP_STATUS=$(curl -s "$API/iati-files/dataworkbench-iati/download/$basename.xml" -o "/workspace/input/$basename.xml" -w "%{http_code}")
 
-# If available: 
-# Run the XML check and the rules
-ant feedback -S -q -Dfilemask=$basename
-
-# Store the result
-
-curl -F "file=@/workspace/dest/$basename.feedback.xml" "$API/iati-files/dataworkbench-iatifeedback/upload"
-
-FILEDATE=$(date -Iseconds -r /workspace/dest/$basename.feedback.xml)
-
-APIDATA="{\"md5\": \"$basename\", \"feedback-updated\": \"$FILEDATE\", \"feedback-version\": \"$VERSION\"}"
-
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
--d "$APIDATA" \
-"$API/iati-datasets/upsertWithWhere?where=%7B%22md5%22%3A%22$basename%22%7D"
-
-# Run the JSON conversion
-
-ant json -S -q -Dfilemask=$basename
-
-# Store the result
-
-curl -F "file=@/workspace/json/$basename.json" "$API/iati-files/dataworkbench-json/upload"
-
-FILEDATE=$(date -Iseconds -r /workspace/json/$basename.json)
-
-APIDATA="{\"md5\": \"$basename\", \"json-updated\": \"$FILEDATE\", \"json-version\": \"$VERSION\"}"
-
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
--d "$APIDATA" \
-"$API/iati-datasets/upsertWithWhere?where=%7B%22md5%22%3A%22$basename%22%7D"
-
-# Run the SVRL conversion
-ant svrl -S -q -Dfilemask=$basename
-
-# Store the result
-
-if xmllint --noout /workspace/svrl/$basename.svrl 2> "/dev/null"; then
-  curl -F "file=@/workspace/svrl/$basename.svrl" "$API/iati-files/dataworkbench-svrl/upload"
-
-  FILEDATE=$(date -Iseconds -r /workspace/svrl/$basename.svrl)
-
-  APIDATA="{\"md5\": \"$basename\", \"svrl-updated\": \"$FILEDATE\", \"svrl-version\": \"$VERSION\"}"
-
+# If available:
+if [[ $HTTP_STATUS == 200 ]]; then 
+  # Run the XML check and the rules
+  ant feedback -S -q -Dfilemask=$basename
+  
+  # Store the result
+  
+  curl -F "file=@/workspace/dest/$basename.feedback.xml" "$API/iati-files/dataworkbench-iatifeedback/upload"
+  
+  FILEDATE=$(date -Iseconds -r /workspace/dest/$basename.feedback.xml)
+  
+  APIDATA="{\"md5\": \"$basename\", \"feedback-updated\": \"$FILEDATE\", \"feedback-version\": \"$VERSION\"}"
+  
   curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
   -d "$APIDATA" \
   "$API/iati-datasets/upsertWithWhere?where=%7B%22md5%22%3A%22$basename%22%7D"
+  
+  # Run the JSON conversion
+  
+  ant json -S -q -Dfilemask=$basename
+  
+  # Store the result
+  
+  curl -F "file=@/workspace/json/$basename.json" "$API/iati-files/dataworkbench-json/upload"
+  
+  FILEDATE=$(date -Iseconds -r /workspace/json/$basename.json)
+  
+  APIDATA="{\"md5\": \"$basename\", \"json-updated\": \"$FILEDATE\", \"json-version\": \"$VERSION\"}"
+  
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+  -d "$APIDATA" \
+  "$API/iati-datasets/upsertWithWhere?where=%7B%22md5%22%3A%22$basename%22%7D"
+  
+  # Run the SVRL conversion
+  ant svrl -S -q -Dfilemask=$basename
+  
+  # Store the result
+  
+  if xmllint --noout /workspace/svrl/$basename.svrl 2> "/dev/null"; then
+    curl -F "file=@/workspace/svrl/$basename.svrl" "$API/iati-files/dataworkbench-svrl/upload"
+  
+    FILEDATE=$(date -Iseconds -r /workspace/svrl/$basename.svrl)
+  
+    APIDATA="{\"md5\": \"$basename\", \"svrl-updated\": \"$FILEDATE\", \"svrl-version\": \"$VERSION\"}"
+  
+    curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+    -d "$APIDATA" \
+    "$API/iati-datasets/upsertWithWhere?where=%7B%22md5%22%3A%22$basename%22%7D"
+  fi
+
 fi
 
 # Remove the files from the local node
-cd /workspace
-rm -rf ${basename}*
+find /workspace -name "${basename}*" -delete
