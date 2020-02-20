8b7c9485295156e0d16ed1472da4f9789ff60acf
Rolf Kleef <rolf@data4development.nl>
2020-02-20 18:27:05 +0100

    Port auto update of identifiers at start
    For now park the known prefixes and publisher ids in the ruleset library

---
+++ update-data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Get the latest versions of data files
+
+API=${API:=none}
+
+if [[ "$API" != "none" ]]; then
+  BUCKET_FILES=${BUCKET_FILES:=dataworkbench-files-staging}
+  BUCKET_FILES_DIR=${BUCKET_FILES_DIR:=dwb}
+
+  for FILE in known-orgid-prefixes.xml known-publishers.xml; do
+    curl -sS -o $FILE $API/iati-files/$BUCKET_FILES/download/${BUCKET_FILES_DIR}_$FILE
+    if [[ $(wc -l <$FILE) -gt 1000 ]]; then
+      mv $FILE lib/iati-rulesets/var
+    else
+      rm $FILE
+    fi
+  done
+fi
